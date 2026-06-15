8248bedcce0d9da81393ab0e482e2ddc7731303e
Syrym <suleimenov.syrym.s@gmail.com>
2026-06-15 14:53:23 +0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-})
+  base: "/driver",
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  },
+  build: {
+    cssCodeSplit: false,
+    lib: {
+      entry: "src/main.jsx",
+      formats: ["iife"],
+      name: "DriverApp",
+      fileName: () => "index.js",
+      cssFileName: "index",
+    },
+  },
+});
