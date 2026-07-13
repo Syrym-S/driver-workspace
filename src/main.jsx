27d29f53fc8014be42c891e42422e522706b711f
ibeksapar <aibek.saparr@gmail.com>
2026-07-13 17:51:15 +0500

    fix: remove leaflet flag

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'
 import 'leaflet/dist/leaflet.css';
 import 'leaflet/dist/leaflet.css';
 import './pages/Trip/components/leaflet-icons.js';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
